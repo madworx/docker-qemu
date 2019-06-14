d966eb5584fec18012e0c23af9d9557c56b6e309
Martin Kjellstrand <martin.kjellstrand@madworx.se>
2019-06-14 16:07:20 +0200

    Travis

--- travis-functions.sh
+++ travis-functions.sh
@@ -9,12 +9,34 @@ function ensure_autoupgrade_branch() {
     git remote remove origin
     git remote add origin https://${GH_TOKEN}@github.com/madworx/docker-qemu
     git checkout autoupgrade || git checkout -b autoupgrade
-    echo "Ensured autoupgrade branch. Now attempting to do a fast-forward merge against ${TRAVIS_BRANCH}."
-    if git merge --ff-only "${TRAVIS_BRANCH}" ; then
-        git push --set-upstream origin autoupgrade
-    else
-        echo "Unable to perform a ff-merge against ${TRAVIS_BRANCH}. Aborting attempt."
+    
+    echo "Ensured autoupgrade branch. Checking if we can do fast-forward merge against ${TRAVIS_BRANCH}."
+    if ! git merge --ff-only "${TRAVIS_BRANCH}" ; then
+        # PR might not exist here.
+        ERRMSG="Unable to perform a ff-merge against ${TRAVIS_BRANCH}. Aborting attempt."
+        comment_on_pr "${ERRMSG}" || echo "${ERRMSG}. Also, no PR exists towards the autoupgrade branch so not logging status there."
+        exit 1
     fi
+}
+
+function merge_autobuild_against_autoupgrade() {
+    git checkout autoupgrade
+    if git merge --ff-only "${TRAVIS_BRANCH}" ; else
+           ERRMSG="Unable to perform a ff-merge against ${TRAVIS_BRANCH}. Aborting attempt."
+           comment_on_pr "${ERRMSG}" || echo "${ERRMSG}. Also, no PR exists towards the autoupgrade branch so not logging status there."
+           comment_on_pr "${ERRMSG}"
+           exit 1
+    fi
+}
+
+function create_pr_if_not_exists() {
+}
+
+function find_pr() {
+}
+
+function comment_on_pr() {
+    
 }
 
 function set_makefile_qemu_version() {
